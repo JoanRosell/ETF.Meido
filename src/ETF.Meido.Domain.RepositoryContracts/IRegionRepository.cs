94a3a9300ba5876aafdbccde73809ef2ba25ff3a
Joan Rosell Rodríguez <joan.rosellr@gmail.com>
2021-12-04 14:56:42 +0100

    feat: add domain entities and repo contracts

---
+++ src/ETF.Meido.Domain.RepositoryContracts/IRegionRepository.cs
@@ -0,0 +1,11 @@
+﻿using ETF.Meido.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ETF.Meido.Domain.RepositoryContracts
+{
+    public interface IRegionRepository
+    {
+        public Task<IEnumerable<Region>> GetRegionsAsync();
+    }
+}
